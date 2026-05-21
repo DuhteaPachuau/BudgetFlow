556b193b4cce953714da82bb9a92fea616963925
pachuau10 <duhteapachuau10@gmail.com>
2026-05-21 22:27:56 +0530

    switch to neon postgresql

--- budgetplanner/settings.py
+++ budgetplanner/settings.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,11 +52,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "budgetplanner.wsgi.application"
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
