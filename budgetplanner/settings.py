c368982ff56a611795904175e00346e75f1306e3
pachuau10 <duhteapachuau10@gmail.com>
2026-05-21 16:37:42 +0530

    fix static url

--- budgetplanner/settings.py
+++ budgetplanner/settings.py
@@ -69,7 +69,7 @@ TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
